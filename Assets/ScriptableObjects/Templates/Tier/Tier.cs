cc318cfc8ff4eb3af28f8938c82f902db4a605e5
Ömer Faruk Saday <sadayomerfaruk@gmail.com>
2026-02-08 16:31:16 +0300

    Some changes that make assigning Tier ScriptableObject assets to Weapon ScriptableObject assets much easier and almost completely automatic

---
+++ Assets/ScriptableObjects/Templates/Tier/Tier.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Tier", menuName = "Loot/Tier")]
+public class Tier : ScriptableObject
+{
+    public int TierNumber;
+}
--- Assets/ScriptableObjects/Templates/Weapon.cs
+++ Assets/ScriptableObjects/Templates/Weapon.cs
@@ -1,11 +1,40 @@
 using UnityEngine;
 
+public enum WeaponType
+{
+    Sword,
+    Knife,
+    Spear,
+    Bow
+}
+
 [CreateAssetMenu(fileName = "Weapon", menuName = "Loot/Weapon")]
 public class Weapon : ScriptableObject
 {
+    public WeaponType Type;
     public string WeaponName;
+    public Tier WeaponTier;
+    public TierDatabase TierDB;
+    public int WeaponTier_Number;
     public float Damage;
     public float FireRate;
     public float Range;
     public GameObject ItemInDroppedForm;
+    public GameObject ProjectilePrefab;
+
+    private void OnValidate()
+    {
+        foreach (var IndividualTier in TierDB.TierList)
+        {
+            if(IndividualTier.TierNumber == WeaponTier_Number)
+            {
+                WeaponTier = IndividualTier;
+            }
+        }
+
+        switch (Type)
+        {
+            case WeaponType.Sword: FireRate = 3; Range = 3; break;
+        }
+    }
 }
